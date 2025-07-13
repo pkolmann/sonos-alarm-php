062b8597556b4af2567e3440d07f8645113f58b7
Philipp Kolmann <philipp.kolmann@fhwn.ac.at>
2025-07-13 14:40:53 +0200

    Update sonos to 2.3.0, test new createAlarm(), add logging.

--- web/api.php
+++ web/api.php
@@ -1,13 +1,24 @@
 <?php
 
-use duncan3dc\Sonos\Exceptions\UnknownGroupException;
+use duncan3dc\Sonos\Exceptions\NotFoundException;
+use duncan3dc\Sonos\Network;
 
 header('Content-Type: application/json');
+
+function logger($message): void
+{
+    $appdir = dirname(__DIR__);
+    $logfile = "$appdir/api.log";
+    $log = new Monolog\Logger("sonos");
+    $log->pushHandler(new Monolog\Handler\StreamHandler($logfile, Monolog\Logger::DEBUG));
+    $log->debug($message);
+}
 
 $appdir = dirname(__DIR__);
 require_once "$appdir/vendor/autoload.php";
 require_once "$appdir/lib/SonosAlarm.php";
 
+$sonos = new Network;
 $sonosAlarm = new SonosAlarm();
 $alarms = $sonosAlarm->getAlarms();
 
@@ -72,7 +83,8 @@ if ($_GET['cmd'] == "toggleAlarm"&& isset($_GET['alarmId'])) {
 }
 
 if (
-    $_GET['cmd'] == "addAlarm"
+    isset($_GET['cmd'])
+    && $_GET['cmd'] == "addAlarm"
     && isset($_GET['room'])
     && isset($_GET['time'])
     && isset($_GET['frequency'])
@@ -80,17 +92,51 @@ if (
     $room = $_GET['room'];
     $time = $_GET['time'];
     $frequency = $_GET['frequency'];
+    logger("Adding alarm in room: $room at time: $time with frequency: $frequency");
 
     try {
+        // find speaker by room
+        $speaker = $sonos->getSpeakerByRoom($room);
+        logger("Found speaker: " . $speaker->getName() . " in room: $room");
+        $newAlarm = $sonos->createAlarm($speaker);
+        logger("Created alarm object for speaker: " . $speaker->getName());
+        $newAlarm->setTime($time);
+        logger("Setting alarm time: $time");
+        $newAlarm->setFrequency($frequency);
+        logger("Setting alarm frequency: $frequency");
+        $newAlarm->setMusic("x-rincon-mp3radio://streaming.radio.co/sd0c4f2b1c/listen");
+        logger("Setting alarm music to streaming.radio.co");
+        $newAlarm->setDuration(600); // 10 minutes
+        logger("Setting alarm duration to 600 seconds (10 minutes)");
+        $newAlarm->setVolume(5); // 20% volume
+        logger("Setting alarm volume to 5 (20%)");
+        $newAlarm->setShuffle(false);
+        logger("Setting alarm shuffle to false");
+        $newAlarm->setRepeat(false);
+        logger("Setting alarm repeat to false");
+        logger("Setting alarm properties: time=$time, frequency=$frequency, music=streaming.radio.co, duration=600, volume=5, shuffle=false, repeat=false");
+        $newAlarm->activate();
+        logger("Alarm activated successfully in room: $room");
+
+    } catch (NotFoundException $e) {
+        $error = [
+            "error" => "Speaker not found in room: $room. Please check the room name. (" . $e->getMessage() . ")"
+        ];
+        print(json_encode($error));
+        logger("Error: " . $error['error']);
+        exit;
+    } catch (Exception $e) {
         $addAlarmRet = $sonosAlarm->addAlarm($room, $time, $frequency);
     } catch (Exception $e) {
         $error = [
-            "error" => $e->getMessage()
+            "error" => "An unknown error occurred while adding the alarm: " . $e->getMessage()
         ];
         print(json_encode($error));
+        logger("Error: " . $error['error']);
+        logger($e);
         exit;
     }
-    print(json_encode($addAlarmRet));
+    print(json_encode($newAlarm));
 }
 
 if ($_GET['cmd'] == "deleteAlarm" && isset($_GET['alarmId'])) {
